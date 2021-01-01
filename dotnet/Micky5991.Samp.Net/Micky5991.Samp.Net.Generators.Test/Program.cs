b1590f856dddd00956f39accdc8f372707f743df
Francesco Paolocci <micky5991@gmail.com>
2021-01-01 23:46:00 +0100

    Add natives idl files

--- dotnet/Micky5991.Samp.Net/Micky5991.Samp.Net.Generators.Test/Program.cs
+++ dotnet/Micky5991.Samp.Net/Micky5991.Samp.Net.Generators.Test/Program.cs
@@ -13,6 +13,10 @@ namespace Micky5991.Samp.Net.Generators.Test
             {
                 Path.GetFullPath("natives.a_samp.idl"),
                 Path.GetFullPath("natives.a_players.idl"),
+                Path.GetFullPath("natives.a_actor.idl"),
+                Path.GetFullPath("natives.a_http.idl"),
+                Path.GetFullPath("natives.a_objects.idl"),
+                Path.GetFullPath("natives.a_vehicles.idl"),
             });
 
             File.WriteAllText("result.cs", result);
