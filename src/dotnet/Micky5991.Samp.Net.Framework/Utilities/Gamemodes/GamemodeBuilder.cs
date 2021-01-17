37a13ca0983d7facade2c1a0861eb0a621bbbd4d
Francesco Paolocci <micky5991@gmail.com>
2021-01-17 13:06:03 +0100

    Fix setup of code analyzer and add some documentation

--- src/dotnet/Micky5991.Samp.Net.Framework/Utilities/Gamemodes/GamemodeBuilder.cs
+++ src/dotnet/Micky5991.Samp.Net.Framework/Utilities/Gamemodes/GamemodeBuilder.cs
@@ -15,8 +15,15 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Micky5991.Samp.Net.Framework.Utilities.Gamemodes
 {
+    /// <summary>
+    /// Type that registers typical core services to the service container.
+    /// </summary>
     public class GamemodeBuilder
     {
+        /// <summary>
+        /// Registers all core services provided by SAMP.Net.
+        /// </summary>
+        /// <param name="serviceCollection">Service collection to register all services to.</param>
         public virtual void AddServices(IServiceCollection serviceCollection)
         {
             this.AddGamemodeStarter(serviceCollection);
@@ -28,32 +35,62 @@ namespace Micky5991.Samp.Net.Framework.Utilities.Gamemodes
             this.AddNatives(serviceCollection);
         }
 
+        /// <summary>
+        /// Should register the gamemode starter that should boot up the gamemode. Override to replace the default
+        /// implementation with your custom starter.
+        /// </summary>
+        /// <param name="serviceCollection">Service collection to add the starter to.</param>
         protected virtual void AddGamemodeStarter(IServiceCollection serviceCollection)
         {
             serviceCollection.AddSingleton<IGamemodeStarter, GamemodeStarter>();
         }
 
+        /// <summary>
+        /// Should register the native samp logger that listens to all samp logs. Override to replace the default
+        /// implementation with your custom listener.
+        /// </summary>
+        /// <param name="serviceCollection">Service collection to add the logger to.</param>
         protected virtual void AddLoggerHandler(IServiceCollection serviceCollection)
         {
             serviceCollection.AddSingleton<ISampLoggerHandler, SampLoggerHandler>();
         }
 
+        /// <summary>
+        /// Should register the used event aggregator. It still needs to implement the interface <see cref="IEventAggregator"/>.
+        /// Override to replace the default implementation with your custom event aggregator.
+        /// </summary>
+        /// <param name="serviceCollection">Service collection to add the event aggregator to.</param>
         protected virtual void AddEventAggregator(IServiceCollection serviceCollection)
         {
             serviceCollection.AddSingleton<IEventAggregator, EventAggregatorService>();
         }
 
+        /// <summary>
+        /// Should register the used event aggregator. It still needs to implement the interface <see cref="IEventAggregator"/>.
+        /// Override to replace the default implementation with your custom event aggregator.
+        /// </summary>
+        /// <param name="serviceCollection">Service collection to add the event aggregator to.</param>
         protected virtual void AddSynchronizationServices(IServiceCollection serviceCollection)
         {
             serviceCollection.AddSingleton<SampSynchronizationContext>();
         }
 
+        /// <summary>
+        /// Add services for native event handling. Override to replace the default implementation with your custom
+        /// event handling.
+        /// </summary>
+        /// <param name="serviceCollection">Service collection to add the event handlers to.</param>
         protected virtual void AddNativeEventHandling(IServiceCollection serviceCollection)
         {
             serviceCollection.AddTransient<NativeTypeConverter>()
                              .AddSingleton<INativeEventRegistry, NativeEventRegistry>();
         }
 
+        /// <summary>
+        /// Registers all default event collections to the service collection. Override to replace the default
+        /// implementation with your custom native event collections.
+        /// </summary>
+        /// <param name="serviceCollection">Service collection to add the event collections to.</param>
         protected virtual void AddNativeEvents(IServiceCollection serviceCollection)
         {
             serviceCollection.AddTransient<INativeEventCollectionFactory, SampEventCollectionFactory>()
@@ -63,6 +100,11 @@ namespace Micky5991.Samp.Net.Framework.Utilities.Gamemodes
                              .AddTransient<INativeEventCollectionFactory, ObjectsEventCollectionFactory>();
         }
 
+        /// <summary>
+        /// Registers all available natives to the service collection seperated by namespace. Override to replace the default
+        /// implementation with your custom natives.
+        /// </summary>
+        /// <param name="serviceCollection">Service collection to add the natives to.</param>
         protected virtual void AddNatives(IServiceCollection serviceCollection)
         {
             serviceCollection.AddTransient<ISampNatives, SampNatives>()
