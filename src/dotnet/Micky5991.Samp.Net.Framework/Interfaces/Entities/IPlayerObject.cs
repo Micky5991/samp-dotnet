9e77adea8c07a614229975153adf27d1077188fc
Francesco Paolocci <micky5991@gmail.com>
2021-01-23 16:05:58 +0100

    Add object attachments for players.

--- src/dotnet/Micky5991.Samp.Net.Framework/Interfaces/Entities/IPlayerObject.cs
+++ src/dotnet/Micky5991.Samp.Net.Framework/Interfaces/Entities/IPlayerObject.cs
@@ -1,7 +1,7 @@
 namespace Micky5991.Samp.Net.Framework.Interfaces.Entities
 {
     /// <summary>
-    /// Represents an object that is positioned relative to a player.
+    /// Represents an object that is only visible to a player.
     /// </summary>
     public interface IPlayerObject : IPlayerBoundEnitity
     {
