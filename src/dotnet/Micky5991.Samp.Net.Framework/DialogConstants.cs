f29ed8c37111a65e12a457f19e0b43a82b5af613
Francesco Paolocci <micky5991@gmail.com>
2021-01-24 13:07:39 +0100

    Add wrapper to show player dialogs

---
+++ src/dotnet/Micky5991.Samp.Net.Framework/DialogConstants.cs
@@ -0,0 +1,23 @@
+namespace Micky5991.Samp.Net.Framework
+{
+    /// <summary>
+    /// Contains useful constants for dialogs.
+    /// </summary>
+    public static class DialogConstants
+    {
+        /// <summary>
+        /// The current maximum id of a dialog.
+        /// </summary>
+        public const int MaxDialogId = sizeof(short) / 2;
+
+        /// <summary>
+        /// Character that can be used for newlines.
+        /// </summary>
+        public const char NewLine = '\n';
+
+        /// <summary>
+        /// Character that has to be used for a tab.
+        /// </summary>
+        public const char Tab = '\t';
+    }
+}
