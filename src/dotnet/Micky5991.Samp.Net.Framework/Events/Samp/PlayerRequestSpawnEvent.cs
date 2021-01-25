19c74864e7152c0d87984737ccdec287d9ef71b3
Francesco Paolocci <micky5991@gmail.com>
2021-01-25 18:56:34 +0100

    Add missing checks for argument values in certain events

--- src/dotnet/Micky5991.Samp.Net.Framework/Events/Samp/PlayerRequestSpawnEvent.cs
+++ src/dotnet/Micky5991.Samp.Net.Framework/Events/Samp/PlayerRequestSpawnEvent.cs
@@ -1,3 +1,5 @@
+using System;
+using Dawn;
 using Micky5991.EventAggregator.Elements;
 using Micky5991.Samp.Net.Framework.Interfaces.Entities;
 
@@ -12,8 +14,14 @@ namespace Micky5991.Samp.Net.Framework.Events.Samp
         /// Initializes a new instance of the <see cref="PlayerRequestSpawnEvent"/> class.
         /// </summary>
         /// <param name="player">Player that requested the spawn.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="player"/> is null.</exception>
+        /// <exception cref="ObjectDisposedException"><paramref name="player"/> was disposed.</exception>
         public PlayerRequestSpawnEvent(IPlayer player)
         {
+            Guard.Argument(player, nameof(player)).NotNull();
+
+            Guard.Disposal(player.Disposed, nameof(player));
+
             this.Player = player;
         }
 
