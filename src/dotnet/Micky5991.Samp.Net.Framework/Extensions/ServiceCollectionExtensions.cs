f1e1ac627df8a7b5669ef0895e93a68881ed4d7c
Francesco Paolocci <micky5991@gmail.com>
2021-02-21 14:11:13 +0100

    Add missing code documentations

--- src/dotnet/Micky5991.Samp.Net.Framework/Extensions/ServiceCollectionExtensions.cs
+++ src/dotnet/Micky5991.Samp.Net.Framework/Extensions/ServiceCollectionExtensions.cs
@@ -1,19 +1,37 @@
+using System;
 using Dawn;
 using Micky5991.Samp.Net.Framework.Utilities.Gamemodes;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Micky5991.Samp.Net.Framework.Extensions
 {
+    /// <summary>
+    /// Extensions used to simplify adding services to the service collection.
+    /// </summary>
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Uses the default gamemode services builder to add services to the given <paramref name="services"/>.
+        /// </summary>
+        /// <param name="services">Collection to add the services to.</param>
+        /// <returns>Passed <paramref name="services"/> instance.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="services"/> is null.</exception>
         public static IServiceCollection AddSampCoreServices(this IServiceCollection services)
         {
             return services.AddSampCoreServices(new GamemodeServicesBuilder());
         }
 
+        /// <summary>
+        /// Uses the given gamemode services builder to add services to the <paramref name="services"/>.
+        /// </summary>
+        /// <param name="services">Collection to add the services to.</param>
+        /// <param name="builder">Builder instance to use for gamemode.</param>
+        /// <returns>Passed <paramref name="services"/> instance.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="builder"/> or <paramref name="services"/> is null.</exception>
         public static IServiceCollection AddSampCoreServices(this IServiceCollection services, GamemodeServicesBuilder builder)
         {
             Guard.Argument(services, nameof(services)).NotNull();
+            Guard.Argument(builder, nameof(builder)).NotNull();
 
             builder.AddAllServices(services);
 
--- src/dotnet/Micky5991.Samp.Net.Framework/Utilities/Gamemodes/GamemodeBuilder.cs
+++ src/dotnet/Micky5991.Samp.Net.Framework/Utilities/Gamemodes/GamemodeBuilder.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Micky5991.Samp.Net.Core;
 using Micky5991.Samp.Net.Framework.Elements.Gamemodes;
 using Micky5991.Samp.Net.Framework.Interfaces;
 using Microsoft.Extensions.Configuration;
@@ -9,37 +8,64 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Micky5991.Samp.Net.Framework.Utilities.Gamemodes
 {
+    /// <summary>
+    /// Builder that prepares the <see cref="IServiceCollection"/> and <see cref="IServiceProvider"/> for the SAMP Gamemode.
+    /// </summary>
     public class GamemodeBuilder
     {
         private readonly IServiceCollection serviceCollection;
 
-        private IStartup startup;
+        private IStartup? startup;
 
         private List<ISampExtension> extensions;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GamemodeBuilder"/> class.
+        /// </summary>
         public GamemodeBuilder()
             : this(new ServiceCollection())
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GamemodeBuilder"/> class.
+        /// </summary>
+        /// <param name="serviceCollection">Service collection used for new services.</param>
         public GamemodeBuilder(IServiceCollection serviceCollection)
         {
             this.serviceCollection = serviceCollection;
+            this.extensions = new List<ISampExtension>();
         }
 
+        /// <summary>
+        /// Specifies the specific type of the startup used to boot up the main gamemode.
+        /// </summary>
+        /// <typeparam name="T">Startup type of the gamemode.</typeparam>
+        /// <returns>Current <see cref="GamemodeBuilder"/> instance.</returns>
         public GamemodeBuilder UseStartup<T>()
             where T : class, IStartup, new()
         {
             return this.UseStartup(new T());
         }
 
-        public GamemodeBuilder UseStartup(IStartup startup)
+        /// <summary>
+        /// Specifies the instance of the startup type used to boot up the main gamemode.
+        /// </summary>
+        /// <param name="newStartup">Instance to use to start gamemode.</param>
+        /// <returns>Current <see cref="GamemodeBuilder"/> instance.</returns>
+        public GamemodeBuilder UseStartup(IStartup newStartup)
         {
-            this.startup = startup;
+            this.startup = newStartup;
 
             return this;
         }
 
+        /// <summary>
+        /// Builds the <see cref="IServiceProvider"/> for this gamemode.
+        /// </summary>
+        /// <param name="args">Passed arguments of this commandline.</param>
+        /// <returns>Created startable <see cref="IGamemode"/> instance.</returns>
+        /// <exception cref="InvalidOperationException"><see cref="startup"/> is null.</exception>
         public virtual IGamemode Build(string[] args)
         {
             if (this.startup == null)
@@ -63,21 +89,21 @@ namespace Micky5991.Samp.Net.Framework.Utilities.Gamemodes
 
             builder.AddCommandLine(args);
 
-            this.startup.SetupConfiguration(builder);
+            this.startup!.SetupConfiguration(builder);
 
             return builder.Build();
         }
 
         private void BuildServices(IConfiguration configuration)
         {
-            this.serviceCollection.AddSingleton(x => this.startup);
+            this.serviceCollection.AddSingleton(_ => this.startup!);
 
             foreach (var extension in this.extensions)
             {
                 extension.RegisterServices(this.serviceCollection, configuration);
             }
 
-            this.startup.RegisterServices(this.serviceCollection, configuration);
+            this.startup!.RegisterServices(this.serviceCollection, configuration);
         }
 
         private void BuildAuthorization(IConfiguration configuration)
@@ -90,7 +116,7 @@ namespace Micky5991.Samp.Net.Framework.Utilities.Gamemodes
                                                            sampExtension.ConfigureAuthorization(x, configuration);
                                                        }
 
-                                                       this.startup.ConfigureAuthorization(x, configuration);
+                                                       this.startup!.ConfigureAuthorization(x, configuration);
                                                    });
         }
     }
