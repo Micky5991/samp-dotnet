79dd4fd06806e51f01f0042ac4252304341cfc77
Francesco Paolocci <micky5991@gmail.com>
2021-06-06 19:37:20 +0200

    Show simple speedometer display.

--- src/dotnet/Micky5991.Samp.Net.Example/Player/Vehicle/Speedometer.cs
+++ src/dotnet/Micky5991.Samp.Net.Example/Player/Vehicle/Speedometer.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Drawing;
+using System.Numerics;
 using System.Timers;
 using Micky5991.EventAggregator.Interfaces;
 using Micky5991.Samp.Net.Core.Interfaces.Interop;
+using Micky5991.Samp.Net.Framework.Elements.TextDraws;
+using Micky5991.Samp.Net.Framework.Enums;
 using Micky5991.Samp.Net.Framework.Events.Samp;
+using Micky5991.Samp.Net.Framework.Extensions;
 using Micky5991.Samp.Net.Framework.Interfaces;
 using Micky5991.Samp.Net.Framework.Interfaces.Entities.Factories;
+using Micky5991.Samp.Net.Framework.Interfaces.TextDraws;
 using Microsoft.Extensions.Logging;
 
 namespace Micky5991.Samp.Net.Example.Player.Vehicle
@@ -31,6 +36,7 @@ namespace Micky5991.Samp.Net.Example.Player.Vehicle
         public void Attach()
         {
             this.eventAggregator.Subscribe<PlayerEnterVehicleEvent>(this.OnPlayerEnterVehicle);
+            this.eventAggregator.Subscribe<PlayerExitVehicleEvent>(this.OnPlayerExitVehicle);
 
             var timer = this.mainTimerFactory.CreateTimer(TimeSpan.FromSeconds(1));
             timer.Elapsed += this.OnTimerElapsed;
@@ -47,6 +53,37 @@ namespace Micky5991.Samp.Net.Example.Player.Vehicle
         private void OnPlayerEnterVehicle(PlayerEnterVehicleEvent eventdata)
         {
             eventdata.Player.SendMessage(Color.GreenYellow, "Enter");
+
+            if (eventdata.Vehicle.TryGetData<ITextDraw>("speedometer", out var speedometer) == false)
+            {
+                speedometer = new TextDraw(
+                                           new Vector2(505, 345),
+                                           "speedometer")
+                {
+                    UseBox = true,
+                    TextFont = TextFont.AharoniBold,
+                    LetterSize = new Vector2(0.2f, 0.9f),
+                    OutlineSize = 1,
+                    BackgroundColor = Color.Black,
+                    TextColor = Color.White,
+                    BoxColor = Color.Black.Transparentize(.5f),
+                    TextSize = new Vector2(600, 500),
+                };
+
+                eventdata.Vehicle.SetData("speedometer", speedometer);
+            }
+
+            eventdata.Player.ShowTextDraw(speedometer);
+        }
+
+        private void OnPlayerExitVehicle(PlayerExitVehicleEvent eventdata)
+        {
+            eventdata.Player.SendMessage(Color.GreenYellow, "Exit");
+
+            if (eventdata.Vehicle.TryGetData<ITextDraw>("speedometer", out var speedometer))
+            {
+                eventdata.Player.HideTextDraw(speedometer);
+            }
         }
     }
 }
