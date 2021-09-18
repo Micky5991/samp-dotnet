a92988cbc6d18364b4ccf84c7cff05d8318df19e
Francesco Paolocci <micky5991@gmail.com>
2021-09-18 20:08:28 +0200

    Add implementation for speedometer

--- src/dotnet/Micky5991.Samp.Net.Example/Player/Vehicle/Speedometer.cs
+++ src/dotnet/Micky5991.Samp.Net.Example/Player/Vehicle/Speedometer.cs
@@ -1,89 +1,203 @@
 using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Numerics;
 using System.Timers;
 using Micky5991.EventAggregator.Interfaces;
-using Micky5991.Samp.Net.Core.Interfaces.Interop;
+using Micky5991.Samp.Net.Core.Natives.Samp;
 using Micky5991.Samp.Net.Framework.Elements.TextDraws;
 using Micky5991.Samp.Net.Framework.Enums;
 using Micky5991.Samp.Net.Framework.Events.Samp;
 using Micky5991.Samp.Net.Framework.Extensions;
 using Micky5991.Samp.Net.Framework.Interfaces;
+using Micky5991.Samp.Net.Framework.Interfaces.Entities;
 using Micky5991.Samp.Net.Framework.Interfaces.Entities.Factories;
+using Micky5991.Samp.Net.Framework.Interfaces.Entities.Pools;
 using Micky5991.Samp.Net.Framework.Interfaces.TextDraws;
-using Microsoft.Extensions.Logging;
 
 namespace Micky5991.Samp.Net.Example.Player.Vehicle
 {
     public class Speedometer : IEntityListener
     {
+        private const bool UseMetricSystem = true;
+
+        private const double DistanceFactor = UseMetricSystem ? 1.609344 : 1;
+
+        private const string DistanceUnit = UseMetricSystem ? "km" : "mi";
+
+        private const string SpeedUnit = DistanceUnit + "/h";
+
         private readonly IEventAggregator eventAggregator;
 
-        private readonly ILogger<Speedometer> logger;
+        private readonly IMainTimerFactory timerFactory;
 
-        private readonly ISampThreadEnforcer threadEnforcer;
+        private readonly IVehiclePool vehiclePool;
 
-        private readonly IMainTimerFactory mainTimerFactory;
+        private readonly IVehicleMeta vehicleMeta;
 
-        public Speedometer(IEventAggregator eventAggregator, ILogger<Speedometer> logger, ISampThreadEnforcer threadEnforcer, IMainTimerFactory mainTimerFactory)
+        private IMainTimer timer;
+
+        public Speedometer(
+            IEventAggregator eventAggregator,
+            IMainTimerFactory timerFactory,
+            IVehiclePool vehiclePool,
+            IVehicleMeta vehicleMeta)
         {
             this.eventAggregator = eventAggregator;
-            this.logger = logger;
-            this.threadEnforcer = threadEnforcer;
-            this.mainTimerFactory = mainTimerFactory;
+            this.timerFactory = timerFactory;
+            this.vehiclePool = vehiclePool;
+            this.vehicleMeta = vehicleMeta;
         }
 
         public void Attach()
         {
-            this.eventAggregator.Subscribe<PlayerEnterVehicleEvent>(this.OnPlayerEnterVehicle);
-            this.eventAggregator.Subscribe<PlayerExitVehicleEvent>(this.OnPlayerExitVehicle);
+            this.eventAggregator.Subscribe<PlayerStateChangeEvent>(this.OnPlayerStateChanged);
 
-            var timer = this.mainTimerFactory.CreateTimer(TimeSpan.FromSeconds(1));
-            timer.Elapsed += this.OnTimerElapsed;
-            timer.Start();
-
+            this.timer = this.timerFactory.CreateTimer(TimeSpan.FromSeconds(0.25));
+            this.timer.Elapsed += this.OnTimerTick;
+            this.timer.Start();
         }
 
-        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        private void OnTimerTick(object sender, ElapsedEventArgs e)
         {
-            this.logger.LogInformation("Timer elapsed");
-            this.threadEnforcer.EnforceMainThread();
-        }
-
-        private void OnPlayerEnterVehicle(PlayerEnterVehicleEvent eventdata)
-        {
-            eventdata.Player.SendMessage(Color.GreenYellow, "Enter");
-
-            if (eventdata.Vehicle.TryGetData<ITextDraw>("speedometer", out var speedometer) == false)
+            foreach (var vehicle in this.vehiclePool.Entities.Values)
             {
-                speedometer = new TextDraw(
-                                           new Vector2(505, 345),
-                                           "speedometer")
+                if (vehicle.Disposed)
                 {
-                    UseBox = true,
-                    TextFont = TextFont.AharoniBold,
-                    LetterSize = new Vector2(0.2f, 0.9f),
-                    OutlineSize = 1,
-                    BackgroundColor = Color.Black,
-                    TextColor = Color.White,
-                    BoxColor = Color.Black.Transparentize(.5f),
-                    TextSize = new Vector2(600, 500),
+                    continue;
+                }
+
+                this.UpdateDistance(vehicle);
+                this.UpdateSpeedometer(vehicle);
+            }
+        }
+
+        private void OnPlayerStateChanged(PlayerStateChangeEvent eventdata)
+        {
+            if (eventdata.NewState is PlayerState.Driver or PlayerState.Passenger)
+            {
+                this.ShowSpeedometer(eventdata.Player);
+
+                return;
+            }
+
+            this.HideSpeedometer(eventdata.Player);
+        }
+
+        private void ShowSpeedometer(IPlayer player)
+        {
+            if (player.VehicleId == null)
+            {
+                return;
+            }
+
+            var vehicle = this.vehiclePool.FindOrDefaultEntity(player.VehicleId.Value);
+            if (vehicle == null)
+            {
+                return;
+            }
+
+            if (vehicle.TryGetData<List<ITextDraw>>("speedometer", out var speedometerParts) == false)
+            {
+                speedometerParts = new List<ITextDraw>
+                {
+                    new TextDraw(
+                                 new Vector2(500, 380),
+                                 "")
+                    {
+                        UseBox = true,
+                        TextFont = TextFont.AharoniBold,
+                        LetterSize = new Vector2(0.2f, 0.9f),
+                        OutlineSize = 1,
+                        BackgroundColor = Color.Black,
+                        TextColor = Color.White,
+                        BoxColor = Color.Black.Transparentize(.5f),
+                        TextSize = new Vector2(600, 500),
+                    },
+                    new TextDraw(
+                                 new Vector2(500, 367),
+                                 this.vehicleMeta.GetVehicleName(vehicle.Model))
+                    {
+                        TextFont = TextFont.BeckettRegular,
+                        LetterSize = new Vector2(0.45f, 1.8f),
+                        OutlineSize = 0,
+                        TextColor = Color.White,
+                        BackgroundColor = Color.Black,
+                        TextSize = new Vector2(620, 300),
+                    }
                 };
 
-                eventdata.Vehicle.SetData("speedometer", speedometer);
+
+                vehicle.SetData("speedometer", speedometerParts);
+                player.SetData("speedometer", speedometerParts);
             }
 
-            eventdata.Player.ShowTextDraw(speedometer);
+            foreach (var speedometerPart in speedometerParts)
+            {
+                player.ShowTextDraw(speedometerPart);
+            }
         }
 
-        private void OnPlayerExitVehicle(PlayerExitVehicleEvent eventdata)
+        private void HideSpeedometer(IPlayer player)
         {
-            eventdata.Player.SendMessage(Color.GreenYellow, "Exit");
-
-            if (eventdata.Vehicle.TryGetData<ITextDraw>("speedometer", out var speedometer))
+            if (player.TryGetData<List<ITextDraw>>("speedometer", out var speedometers) == false)
             {
-                eventdata.Player.HideTextDraw(speedometer);
+                return;
             }
+
+            foreach (var speedometer in speedometers)
+            {
+                player.HideTextDraw(speedometer);
+            }
+        }
+
+        private void UpdateSpeedometer(IVehicle vehicle)
+        {
+            if (vehicle.TryGetData("speedometer", out IList<ITextDraw> speedometers) == false)
+            {
+                return;
+            }
+
+            if (vehicle.TryGetData("distance", out double distance) == false)
+            {
+                distance = 0;
+            }
+
+            var speedometer = speedometers.First();
+
+            var speed = vehicle.Velocity.Length() * 100 * DistanceFactor;
+            var health = (vehicle.Health - 250) / 750;
+            var convertedDistance = distance / 1000 * (1 / DistanceFactor);
+
+            speedometer.Text = string.Join(
+                                           "~n~",
+                                           "",
+                                           $"~g~~h~ Speed: ~s~{Math.Floor(speed):F0} {SpeedUnit}",
+                                           $"~r~ Health: ~s~{health * 100:F1}%",
+                                           $"~s~ Distance: {convertedDistance:N2} {DistanceUnit}");
+        }
+
+        private void UpdateDistance(IVehicle vehicle)
+        {
+            if (vehicle.TryGetData("distance", out double distance) == false)
+            {
+                distance = 0;
+            }
+
+            if (vehicle.TryGetData("distance_position", out Vector3 lastPosition) == false)
+            {
+                vehicle.SetData("distance_position", vehicle.Position);
+
+                return;
+            }
+
+            var currentPosition = vehicle.Position;
+
+            distance += (currentPosition - lastPosition).Length();
+
+            vehicle.SetData("distance_position", currentPosition);
+            vehicle.SetData("distance", distance);
         }
     }
 }
