b19e97c59a9821ecc69db7627ca3f6166fc58f53
Francesco Paolocci <micky5991@gmail.com>
2021-10-23 20:28:30 +0200

    Prepare quest tasks

---
+++ src/dotnet/Micky5991.Samp.Net.Example/Quests/QuestEventConditionTaskNode.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using JetBrains.Annotations;
+using Micky5991.EventAggregator.Interfaces;
+using Micky5991.Quests.Entities;
+using Micky5991.Quests.Interfaces.Nodes;
+
+namespace Micky5991.Samp.Net.Example.Quests
+{
+    public abstract class QuestEventConditionTaskNode : QuestConditonNode
+    {
+        private IImmutableSet<ISubscription> eventSubscriptions;
+
+        protected QuestEventConditionTaskNode([NotNull] IQuestRootNode rootNode)
+            : base(rootNode)
+        {
+        }
+
+        protected abstract IEnumerable<ISubscription> GetEventSubscriptions();
+
+        protected override void AttachEventListeners()
+        {
+            this.DetachEventListeners();
+
+            this.eventSubscriptions = this.GetEventSubscriptions().ToImmutableHashSet();
+        }
+
+        protected override void DetachEventListeners()
+        {
+            var subscriptions = this.eventSubscriptions;
+            this.eventSubscriptions = ImmutableHashSet<ISubscription>.Empty;
+
+            foreach (var subscription in subscriptions)
+            {
+                subscription.Dispose();
+            }
+        }
+    }
+}
